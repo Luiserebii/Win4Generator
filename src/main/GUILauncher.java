7f7a37507fc266e331a6947198745efa6bf3d37d
Luiserebii <luis@serebii.io>
2016-06-12 14:07:37 -0400

    Refactored GUILauncher

--- src/main/GUILauncher.java
+++ src/main/GUILauncher.java
@@ -8,26 +8,20 @@ import javafx.stage.Stage;
 public class GUILauncher {
 
 	public void updatePopup(){
-		try {
-			Stage s = new Stage();
-			Parent root = FXMLLoader.load(getClass().getResource("../view/UpdateGUI.fxml"));
-			Scene scene = new Scene(root);
-			scene.getStylesheets().add(getClass().getResource("../view/application.css").toExternalForm());
-			s.setTitle("Updating...");
-			s.setScene(scene);
-			s.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		popup("../view/UpdateGUI.fxml", "../view/application.css", "Updating...");
 	}
 
 	public void settingsPopup(){
+		popup("../view/SettingsGUI.fxml", "../view/application.css", "Settings");
+	}
+
+	public void popup(String fxmlPath, String cssPath, String title){
 		try {
 			Stage s = new Stage();
-			Parent root = FXMLLoader.load(getClass().getResource("../view/SettingsGUI.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource(fxmlPath));
 			Scene scene = new Scene(root);
-			scene.getStylesheets().add(getClass().getResource("../view/application.css").toExternalForm());
-			s.setTitle("Settings");
+			scene.getStylesheets().add(getClass().getResource(cssPath).toExternalForm());
+			s.setTitle(title);
 			s.setScene(scene);
 			s.show();
 		} catch(Exception e) {
