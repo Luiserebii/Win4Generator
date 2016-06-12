45aa7870a29d8382e26fdd48cf02c8767c0f8d3e
Luiserebii <luis@serebii.io>
2016-06-12 13:16:59 -0400

    Functionality of settings works perfectly! Moving on to refactoring "inline" CSS in Controller and move to application.css

---
+++ src/application/SettingsGUIController.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+
+public class SettingsGUIController implements Initializable {
+
+	@FXML
+	private CheckBox orderMattersBox;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@FXML
+	public void toggleOrderMatters(){
+		if(orderMattersBox.isSelected()) { //technically, could refactor this into setting isOrderMatters to isSelected, but will keep this way for readibility
+			Win4Generator.isOrderMatters = true;
+		} else {
+			Win4Generator.isOrderMatters = false;
+		}
+	}
+
+
+
+}
